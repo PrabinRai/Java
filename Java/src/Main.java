cd45ddf1ad4a8929c62381abc63341f586e93c91
PrabinRai <chokhang777@gmail.com>
2015-01-20 13:32:05 +0545

    Lab 1

---
+++ Java/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t'  '");
+        System.out.println("\t\t\t __");
+        System.out.println("\t\t\t(\u00A9\u00A9)");
+        System.out.println("\t/========\\/");
+        System.out.println("   / || %% ||");
+        System.out.println("  *  ||----||");
+        System.out.println("     \u00A5\u00A5    \u00A5\u00A5  ");
+        System.out.println("     \"\"    \"\" ");
+
+
+    }
+}
